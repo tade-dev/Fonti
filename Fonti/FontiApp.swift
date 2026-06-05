eeed0ec03de89eee40b0162a2707ff72da485c87
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-06-05 14:38:02 +0100

    Initial Commit

---
+++ Fonti/FontiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontiApp.swift
+//  Fonti
+//
+//  Created by BSTAR on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct FontiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
